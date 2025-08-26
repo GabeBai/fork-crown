905cbd26aa1e40a088924887722cdc5202b85cb5
Gabe <yubobai38@gmail.com>
2025-08-26 09:59:57 -0700

    feat : change stastic

--- crates/analysis/src/statistics.rs
+++ crates/analysis/src/statistics.rs
@@ -26,9 +26,6 @@ pub struct CrateStatistics {
 impl CrateStatistics {
     pub fn new(
         crate_data: &CrateData,
-        fatness_result: &FatnessResult,
-        mutability_result: &MutabilityResult,
-        ownership_result: &SolidifiedOwnershipSchemes,
     ) -> Self {
         let mut statistics = CrateStatistics::default();
 
@@ -39,32 +36,30 @@ impl CrateStatistics {
             // gather ptr count
             for (local, local_decl) in body.local_decls.iter_enumerated() {
                 if local_decl.is_user_variable() && local_decl.ty.is_unsafe_ptr() {
-                    let is_owning =
-                        ownership_result.fn_results(&did).local_result(local)[0].is_owning();
-                    let is_mutable =
-                        mutability_result.fn_results(&did).local_result(local)[0].is_mutable();
-                    let is_thin = fatness_result.fn_results(&did).local_result(local)[0].is_ptr();
-                    if is_mutable && is_thin {
-                        statistics.num_non_arr_mut_unsafe_ptrs += 1;
-                        statistics.num_mut_unsafe_ptrs += 1;
-                        statistics.num_non_arr_unsafe_ptrs += 1;
-                    } else if is_mutable {
-                        statistics.num_mut_unsafe_ptrs += 1;
-                    } else {
-                        statistics.num_non_arr_unsafe_ptrs += 1;
-                    }
+                    // let is_owning =
+                    //     ownership_result.fn_results(&did).local_result(local)[0].is_owning();
+                    // let is_mutable =
+                    //     mutability_result.fn_results(&did).local_result(local)[0].is_mutable();
+                    // let is_thin = fatness_result.fn_results(&did).local_result(local)[0].is_ptr();
+                    // if is_mutable && is_thin {
+                    //     statistics.num_non_arr_mut_unsafe_ptrs += 1;
+                    //     statistics.num_mut_unsafe_ptrs += 1;
+                    //     statistics.num_non_arr_unsafe_ptrs += 1;
+                    // } else if is_mutable {
+                    //     statistics.num_mut_unsafe_ptrs += 1;
+                    // } else {
+                    //     statistics.num_non_arr_unsafe_ptrs += 1;
+                    // }
                     statistics.num_unsafe_ptrs += 1;
-                    if is_owning {
-                        statistics.num_owning_ptrs_detected += 1;
-                    }
+                    // if is_owning {
+                    //     statistics.num_owning_ptrs_detected += 1;
+                    // }
                 }
             }
 
             // gather unsafe usages count
             CountUnsafeUsages {
                 body: &body,
-                fatness_result,
-                mutability_result,
                 statistics: &mut statistics,
             }
             .visit_body(&body);
@@ -76,8 +71,6 @@ impl CrateStatistics {
 
 struct CountUnsafeUsages<'me, 'tcx> {
     body: &'me Body<'tcx>,
-    fatness_result: &'me FatnessResult,
-    mutability_result: &'me MutabilityResult,
     statistics: &'me mut CrateStatistics,
 }
 
@@ -95,20 +88,6 @@ impl<'me, 'tcx> Visitor<'tcx> for CountUnsafeUsages<'me, 'tcx> {
             && place.is_indirect()
             && self.body.local_decls[place.local].ty.is_unsafe_ptr()
         {
-            let did = self.body.source.def_id();
-            let local = place.local;
-            let is_mutable =
-                self.mutability_result.fn_results(&did).local_result(local)[0].is_mutable();
-            let is_thin = self.fatness_result.fn_results(&did).local_result(local)[0].is_ptr();
-            if is_mutable && is_thin {
-                self.statistics.num_non_arr_mut_unsafe_usages += 1;
-                self.statistics.num_mut_unsafe_usages += 1;
-                self.statistics.num_non_arr_unsafe_usages += 1;
-            } else if is_mutable {
-                self.statistics.num_mut_unsafe_usages += 1;
-            } else if is_thin {
-                self.statistics.num_non_arr_unsafe_usages += 1;
-            }
             self.statistics.num_unsafe_usages += 1;
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -336,42 +336,39 @@ fn run(cmd: Command, tcx: TyCtxt<'_>) -> Result<()> {
             ownership_result.print_results(&input);
         }
         Command::Analyse { results_path } => {
-            let alias_result = alias::alias_results(&input);
-            let mutability_result =
-                analysis::type_qualifier::flow_insensitive::mutability::mutability_analysis(&input);
-            let output_params = analysis::type_qualifier::output_params::compute_output_params(
-                &input,
-                &alias_result,
-                &mutability_result,
-            );
-            let crate_ctxt = CrateCtxt::new(&input);
-            let ownership_schemes =
-                analysis::ownership::whole_program::WholeProgramAnalysis::analyze(
-                    crate_ctxt,
-                    &output_params,
-                )?;
+            // let alias_result = alias::alias_results(&input);
+            // let mutability_result =
+            //     analysis::type_qualifier::flow_insensitive::mutability::mutability_analysis(&input);
+            // let output_params = analysis::type_qualifier::output_params::compute_output_params(
+            //     &input,
+            //     &alias_result,
+            //     &mutability_result,
+            // );
+            // let crate_ctxt = CrateCtxt::new(&input);
+            // let ownership_schemes =
+            //     analysis::ownership::whole_program::WholeProgramAnalysis::analyze(
+            //         crate_ctxt,
+            //         &output_params,
+            //     )?;
 
-            let ownership_result = ownership_schemes.solidify(&input);
+            // let ownership_result = ownership_schemes.solidify(&input);
 
-            let fatness_result =
-                analysis::type_qualifier::flow_insensitive::fatness::fatness_analysis(
-                    &input,
-                    &ownership_result,
-                );
+            // let fatness_result =
+            //     analysis::type_qualifier::flow_insensitive::fatness::fatness_analysis(
+            //         &input,
+            //         &ownership_result,
+            //     );
 
             if let Some(results_path) = results_path {
-                let fatness_data = serde_json::to_string(&fatness_result.make_data(&input))?;
-                let mutability_data = serde_json::to_string(&mutability_result.make_data(&input))?;
-                let ownership_data = serde_json::to_string(&ownership_result.make_data(&input))?;
-                fs::write(results_path.join("fatness.json"), fatness_data)?;
-                fs::write(results_path.join("mutability.json"), mutability_data)?;
-                fs::write(results_path.join("ownership.json"), ownership_data)?;
+                // let fatness_data = serde_json::to_string(&fatness_result.make_data(&input))?;
+                // let mutability_data = serde_json::to_string(&mutability_result.make_data(&input))?;
+                // let ownership_data = serde_json::to_string(&ownership_result.make_data(&input))?;
+                // fs::write(results_path.join("fatness.json"), fatness_data)?;
+                // fs::write(results_path.join("mutability.json"), mutability_data)?;
+                // fs::write(results_path.join("ownership.json"), ownership_data)?;
                 let statistics =
                     serde_json::to_string(&analysis::statistics::CrateStatistics::new(
                         &input,
-                        &fatness_result,
-                        &mutability_result,
-                        &ownership_result,
                     ))?;
                 fs::write(results_path.join("statistics.json"), statistics)?;
             }
@@ -415,9 +412,7 @@ fn run(cmd: Command, tcx: TyCtxt<'_>) -> Result<()> {
                 let statistics =
                     serde_json::to_string(&analysis::statistics::CrateStatistics::new(
                         &input,
-                        &fatness_result,
-                        &mutability_result,
-                        &ownership_result,
+
                     ))?;
                 fs::write(results_path.join("statistics.json"), statistics)?;
             }
